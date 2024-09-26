78f9de1633b88dc4d62093ec9b9751b336fd8115
Mugisha Isaie <132471136+mugishaisaie@users.noreply.github.com>
2024-09-26 15:25:41 +0200

    updating title

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import { useState } from "react"
 import questions from "./data"
 import Questions from "./Questions"
+
 function App() {
   const [question, setQuestion] = useState(0)
   const[activeId, setActiveId] = useState(null)
